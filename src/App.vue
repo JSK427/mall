83b4994e57c44fe37c0ebf06660f5dc22669c7df
JSK427 <1484710039@qq.com>
2022-11-23 14:44:09 +0800

    common

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
-	<div id="app"></div>
+  <div id="app">
+    <router-view></router-view>
+    <MainTabBar></MainTabBar>
+  </div>
 </template>
 
-<style lang="less">
+<script>
+import MainTabBar from "components/content/mainTabBar/MainTabBar";
+export default {
+  name: "app",
+  components: {
+    MainTabBar,
+  },
+};
+</script>
+
+<style>
 @import url(assets/css/base.css);
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,20 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-	transpileDependencies: true,
-	//配置webpack config
-	configureWebpack: {
-		//路径相关
-		resolve: {
-			//路径文件后缀名可省略
-			extensions: [],
-			//别名
-			alias: {
-				assets: "@/assets",
-				common: "@/common",
-				components: "@/components",
-				network: "@/network",
-			},
-		},
-	},
+  transpileDependencies: true,
+  //配置webpack config
+  configureWebpack: {
+    //路径相关
+    resolve: {
+      //路径文件后缀名可省略
+      extensions: [],
+      //别名
+      alias: {
+        assets: "@/assets",
+        common: "@/common",
+        components: "@/components",
+        network: "@/network",
+        views: "@/views",
+      },
+    },
+  },
 });
