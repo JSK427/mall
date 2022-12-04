df085305a0c82053cb6b4a803150f0862c2f19af
JSK427 <1484710039@qq.com>
2022-12-05 00:11:26 +0800

    request-and-swiper

---
+++ src/network/request.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export function request(config) {
+  //创建axios实例
+  const instance = axios.create({
+    baseURL: "http://123.207.32.32:8000",
+    timeout: 5000,
+  });
+  //axios的拦截器
+  //请求拦截的作用 成功/失败
+  instance.interceptors.request.use(
+    (config) => {
+      //拦截成功要返回
+      return config;
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
+  //发送真正网络请求
+  return instance(config);
+}
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,49 @@
 <template>
-  <div>
-    <h2>首页</h2>
+  <div id="home">
+    <NavBar class="home-nav">
+      <div slot="center">购物街</div>
+    </NavBar>
+    <HomeSwiper :banners="banners"></HomeSwiper>
+
+
   </div>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar";
+import { getHomeMultidata } from "network/home";
+import HomeSwiper from "./childComps/HomeSwiper";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    NavBar,
+    HomeSwiper
+
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: []
+    }
+  },
+  created() {
+    getHomeMultidata().then(res => {
+      this.banners = res.data.data.banner.list;
+      this.recommends = res.data.data.recommend.list;
+      // console.log(res);
+
+    }).catch(err => {
+
+    })
+
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
+</style>
